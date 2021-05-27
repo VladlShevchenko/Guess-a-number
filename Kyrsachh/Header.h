e72a844ffa6495a3a9978af729fb35153bcc5955
taras-fid <81877511+taras-fid@users.noreply.github.com>
2021-05-27 12:03:54 +0300

    Update Header.h

--- Kyrsachh/Header.h
+++ Kyrsachh/Header.h
@@ -1,11 +1,11 @@
 #pragma once
-#include <iostream> // заголовочный файл с классами, функциями и переменными для организации ввода-вывода в языке программирования C++
-#include <cstdlib> // заголовочный файл стандартной библиотеки языка С++, который содержит в себе функции, занимающиеся выделением памяти, контролем процесса выполнения программы, преобразованием типов и тд
-#include <ctime> // Этот заголовочный файл содержит функции для работы со временем и датой в С++
-#include <iomanip> // заголовочный файл, который содержит модификатор setw. С его помощью можно, как например на 60 и 101 строчке, установить ширину сдвига вывода поля.
-#include <windows.h> // windows.h является Windows-конкретный файл заголовков для языка C++, который содержит заявления для всех функций в Windows API, все общие макросы, которые используются программистами окон, и все типы данных, используемых различными функциями и подсистем.
-#include <mmsystem.h> // Windows Multimedia (файл ребёнок windows.h)
-#pragma comment(lib, "winmm") // это директива компилятора, которая указывает Visual C++ оставить комментарий в сгенерированном объектном файле.
+#include <iostream> // заголовочний файл з класами, функціями і змінними для організації введення-виведення в мові програмування C++
+#include <cstdlib> // заголовочний файл стандартної бібліотеки мови С++, який містить в собі функції, які займаються виділенням пам'яті, контролем процесу виконання програми, перетворенням типів і тд
+#include <ctime> // Цей заголовочний файл містить функції для роботи з часом і датою в С++
+#include <iomanip> // заголовочний файл, який містить модифікатор setw. З його допомогою можна, як наприклад на 60 і 101 сходинці, встановити ширину зсуву виведення поля
+#include <windows.h> // windows.h є Windows-конкретним файлом заголовків для мови C++, який містить заголовки для всіх функцій в Windows API, всі загальні макроси, які використовуються програмістами Windows, і всі типи даних, що використовуються різними функціями і підсистем
+#include <mmsystem.h> // Windows Multimedia (файл-дитина windows.h)
+#pragma comment(lib, "winmm") // це директива компілятора, яка вказує Visual C++ залишити коментар в створеному об'єктному файлі
 
 using namespace std;
 
@@ -14,32 +14,31 @@ using namespace std;
 #define RED 4
 #define WHITE 15
 static const long LONG__MAX = 9223372036854775807;
-static const int  MAX_NUMBER = 100;
-static const int  MIN_NUMBER = 1;
+static const int  MAX_NUMBER = 100;							//  максимальне загадане число
+static const int  MIN_NUMBER = 1;							//  мінімальне загадане число
 
 // 
-class Game {
+class Game {												// клас з ф-ціями інтерфейсу гри
 protected:
-	void play_intro();
-	void play_no();
-	void play_congatulation();
-	
+	void play_intro();										// ф-ція програвання звуку початку гри
+	void play_no();											// ф-ція програвання звуку невдачної спроби вгадати число
+	void play_congatulation();								// ф-ція програвання звуку перемоги
+
 public:
-	void set_console_output();
-	void get_game_time(time_t start, time_t end);
+	void set_console_output();								// ф-ція української локалізації
+	void get_game_time(time_t start, time_t end);			// ф-ція для отримання часу проведеного в грі
 
 };
 
 // 
-class Menu : public Game {
+class Menu : public Game {									// клас-нащадок класу Game для вибору режиму гри
 public:
-	void choose_mode(int& mode);
+	void choose_mode(int& mode);							// ф-ція для вибору режиму гри
 };
 
 //
-class GameMode : public Game {
+class GameMode : public Game {								// клас-нащадок класу Game для режиму гри
 public:
-	void play_with_computer();
-	void play_with_friend();
+	void play_with_computer();								// ція для режиму гри з комп'ютером
+	void play_with_friend();								// ція для режиму гри з іншим користувачем
 };
-
